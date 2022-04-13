5d6f4f986ccdad18d0cdd6e90d6968a5a7eda907
Safia Nassiri <niejelandrew@gmail.com>
2022-04-13 18:29:26 -0400

    Added print statements and loop
    Added a print statements and loop to test Git and Github.

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      for(int i = 1; i<=100; i++)
+      {
+         System.out.println(i);
+      }
+   }//end of main
+}//end of class
